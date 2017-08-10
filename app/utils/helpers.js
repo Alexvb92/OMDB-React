63d8a2f3430a5505c5099b1f3adb9fcbf94f7bd1
alexvb92 <a_vonbredow@live.com>
2017-08-10 14:41:01 -0400

    i added dsome stuff

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,7 +1,7 @@
 var axios = require('axios');
 
 function getmovieinfo(moviename){
-  return axios.get('https://www.omdbapi.com/?t=' + moviename + '&plot=full');
+  return axios.get('https://www.omdbapi.com/?apikey=31c084ce&?t=' + moviename + '&plot=full');
 };
 
 
